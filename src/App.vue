fc41b7214b977eff753b394d4bdd34e6d790d625
CeazzZY <1093918683@qq.com>
2024-02-18 18:40:04 +0800

    feat: style

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,119 @@
-<script setup></script>
+<script lang="ts" setup>
+import { reactive } from "vue";
+
+const p1 = reactive({
+  HP: 5,
+});
+
+const p2 = reactive({
+  HP: 5,
+});
+</script>
 
 <template>
-  <div></div>
+  <div className="main">
+    <div className="left"></div>
+    <div className="right"></div>
+
+    <button className="centerButton">开始游戏</button>
+    <button className="chooseLeft">p1</button>
+    <button className="chooseRight">p2</button>
+    <div className="p1Tools">
+      <div></div>
+      <div></div>
+    </div>
+    <div className="p2Tools"></div>
+    <div className="p1HP">{{ p1.HP }}</div>
+    <div className="p2HP">{{ p2.HP }}</div>
+    <div className="remind">p1选择</div>
+  </div>
 </template>
+
+<style>
+body {
+  height: 100%;
+  width: 100%;
+  margin: 0;
+  padding: 0;
+}
+
+.main {
+  position: relative;
+  width: 100vw;
+  height: 100vh;
+}
+
+.centerButton {
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+}
+
+.chooseLeft {
+  position: absolute;
+  top: 50%;
+  left: 30%;
+  transform: translate(0, -50%);
+}
+
+.chooseRight {
+  position: absolute;
+  top: 50%;
+  right: 30%;
+  transform: translate(0, -50%);
+}
+
+.p1HP {
+  position: absolute;
+  top: 10%;
+  left: 30%;
+}
+
+.p2HP {
+  position: absolute;
+  top: 10%;
+  right: 30%;
+}
+
+.p1Tools {
+  position: absolute;
+  top: 10%;
+  left: 0;
+  width: 18vw;
+  height: 80vh;
+  border: 1px solid black;
+}
+
+.p2Tools {
+  position: absolute;
+  top: 10%;
+  right: 0;
+  width: 18vw;
+  height: 80vh;
+  border: 1px solid black;
+}
+
+.remind {
+  position: absolute;
+  left: 50%;
+  top: 10%;
+  transform: translate(-50%, 0);
+}
+
+.left {
+  position: absolute;
+  top: 0;
+  left: 0;
+  height: 100%;
+  width: 50%;
+}
+
+.right {
+  position: absolute;
+  top: 0;
+  right: 0;
+  height: 100%;
+  width: 50%;
+}
+</style>
